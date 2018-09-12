c8a14fff04bceb861a6a7cdd76be349aa4a848bf
Aleksandra Katonina <aleksandra.katonina@klarna.com>
2018-09-12 18:18:04 +0200

    First iteration of this project

--- README.md
+++ README.md
@@ -1,6 +1,36 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+The script reads a text file, splits it by space and returns 10 most frequent words.
+
+
+### Description
+* Works with any language that divides words with spacings (e.g. won't work with Chinese)
+* Will return an error if fails to read file
+* Uses only word characters for analysis
+
+### How to launch
+
+Example of script launch on Linux, Python 3.5:
+
+```
+bash
+
+$ python lang_frequency.py <filepath>
+# possibly requires call of python3 executive instead of just python
+Top 10 most frequent words in file:
+the: 192
+of: 112
+i: 91
+and: 62
+my: 61
+in: 50
+a: 49
+to: 47
+had: 40
+was: 39
+```
+
+### Project goals
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,62 @@
+import argparse
+import re
 
+
+def get_parser_args():
+    parser = argparse.ArgumentParser(
+        description='Input path to the analysed file')
+
+    parser.add_argument(
+        'filepath',
+        help='Path to text file')
+
+    args = parser.parse_args()
+    return args
 
 def load_data(filepath):
-    pass
+    file = open(filepath)
+    text_str = file.read()
+    return text_str
 
 
-def get_most_frequent_words(text):
-    pass
+def remove_punctuation(text_str):
+    clean_text_str = re.sub(r'[^\w]', ' ', text_str)
+    return clean_text_str
+
+def tokenize(text_str):
+    if text_str is not None:
+        wordlist = text_str.lower().split()
+        return wordlist
+    else:
+        return None
+
+def get_words_frequency_dict(wordlist):
+    wordfreq = [wordlist.count(p) for p in wordlist]
+    frequency_dict = dict(zip(wordlist, wordfreq))
+    return frequency_dict
+
+
+def get_top_10_frequent_words(frequency_dict):
+    top_10_list = sorted(
+        frequency_dict.items(),
+        key=lambda x: x[1],
+        reverse = True)[:10]
+    return top_10_list
+
+
+def pretty_print_list_of_tuples(top_10_list):
+    print('Top 10 most frequent words in file:')
+    for i in top_10_list:
+        print('{}: {}'.format(i[0], i[1]))
 
 
 if __name__ == '__main__':
-    pass
+    args = get_parser_args()
+    text_str = load_data(args.filepath)
+    if text_str is None:
+        exit('Failed to load file')
+    clean_text_str = remove_punctuation(text_str)
+    wordlist = tokenize(clean_text_str)
+    frequency_dict = get_words_frequency_dict(wordlist)
+    top_10_list = get_top_10_frequent_words(frequency_dict)
+    pretty_print_list_of_tuples(top_10_list)
